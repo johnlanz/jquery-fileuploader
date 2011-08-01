8f474491a10d46f3a00e76f05fb176c3eb53de75
John Laniba <johnlanz@live.com>
2011-08-01 23:41:59 +0800

    - handles other form input  - added beforeUpload, beforeEachUpload, afterUpload, and afterEachUpload callbacks  - removed callback, inputName, and inputSize config  - can now multiple script initialization in a single page

--- jquery.fileUploader.js
+++ jquery.fileUploader.js
@@ -1,170 +1,330 @@
 /*
 *	Class: fileUploader
-*	Use: Upload multiple files the jQuery way
-*	Author: John Lanz (http://pixelcone.com)
-*	Version: 1.0
+*	Use: Upload multiple files using your native form
+*	Author: John Laniba (http://pixelcone.com)
+*	Version: 1.2
 */
 
 (function($) {
-	$.fileUploader = {version: '1.1'};
+	$.fileUploader = {version: '1.2', count: 0};
 	$.fn.fileUploader = function(config){
 		
 		config = $.extend({}, {
-			limit: '',
+			limit: false,
 			imageLoader: '',
 			buttonUpload: '#pxUpload',
 			buttonClear: '#pxClear',
 			successOutput: 'File Uploaded',
 			errorOutput: 'Failed',
-			inputName: 'userfile',
-			inputSize: 30,
 			allowedExtension: 'jpg|jpeg|gif|png',
-			callback: function() {
-				
-			}
+			
+			//Callbacks
+			onFileChange: function(){},
+			onFileRemove: function(){},
+			beforeUpload: function(){}, //trigger after the submit button is click: before upload
+			beforeEachUpload: function(){}, //callback before each file has been uploaded ::: returns each Form
+			afterUpload: function(){},
+			afterEachUpload: function(){}, //callback after each file has been uploaded
+			
 		}, config);
 		
-		var itr = 0; //number of files to uploaded
-		var $limit = 1;
+		$.fileUploader.count++;
 		
-		//public function
-		$.fileUploader.change = function(e){
-			var fname = px.validateFile( $(e).val() );
-			if (fname == -1){
-				alert ("Invalid file!");
-				$(e).val("");
-				return false;
-			}
-			$('#px_button input').removeAttr("disabled");
-			var imageLoader = '';
-			if ($.trim(config.imageLoader) != ''){
-				imageLoader = '<img src="'+ config.imageLoader +'" alt="uploader" />';
-			}
-			var display = '<div class="uploadData" id="pxupload'+ itr +'_text" title="pxupload'+ itr +'">' + 
-				'<div class="close">&nbsp;</div>' +
-				'<span class="fname">'+ fname +'</span>' +
-				'<span class="loader" style="display:none">'+ imageLoader +'</span>' +
-				'<div class="status">Pending...</div></div>';
+		var pxUploadForm = 'pxupload_form' + $.fileUploader.count;
+		var pxButton = 'px_button' + $.fileUploader.count;
+		var wrapper = ' \
+			<div id="'+ pxUploadForm +'"></div> \
+			<div id="'+ pxButton +'"></div> \
+		';
+		pxUploadForm = '#' + pxUploadForm;
+		pxButton = '#' + pxButton;
+		
+		var itr = 1; //index/itr of file
+		var isLimit = (config.limit)? true : false;
+		var limit = parseInt(config.limit);
+		
+		var e = this; //set e as this
+		var selector = $(this).selector;
+		var buttonM = pxButton + ' input, '+ pxButton +' button'; //Accept button as input and as button
+		var isFile = false; //Var declaration
+		
+		var px = {
 			
-			$("#px_display").append(display);
-			if (config.limit == '' || $limit < config.limit) {
-				px.appendForm();
-			}
-			$limit++;
-			$(e).hide();
-		}
-		
-		$(config.buttonUpload).click(function(){
-			if (itr > 1){
-				$('#px_button input').attr("disabled","disabled");
-				$("#pxupload_form form").each(function(){
-					e = $(this);
-					var id = "#" + $(e).attr("id");
-					var input_id = id + "_input";
-					var input_val = $(input_id).val();
-					if (input_val != ""){
-						$(id + "_text .status").text("Uploading...");
-						$(id + "_text").css("background-color", "#FFF0E1");
-						$(id + "_text .loader").show();
-						$(id + "_text .close").hide();
-						
-						$(id).submit();
-						$(id +"_frame").load(function(){
-							$(id + "_text .loader").hide();
-							up_output = $(this).contents().find("#output").text();
-							if (up_output == "success"){
-								$(id + "_text").css("background-color", "#F0F8FF");
-								up_output = config.successOutput;
-							}else{
-								$(id + "_text").css("background-color", "#FF0000");
-								up_output = config.errorOutput;
-							}
-							up_output += '<br />' + $(this).contents().find("#message").text();
-							$(id + "_text .status").html(up_output);
-							$(e).remove();
-							$(config.buttonClear).removeAttr("disabled");
-							config.callback($(this));
-						});
+			//Initialize and format data
+			init: function(){
+				px.form = $(e).parents('form');
+				
+				//prepend wrapper markup
+				px.form.before(wrapper);
+				
+				//clear all form data
+				px.clearFormData(px.form);
+				
+				//move upload and clear button into id px_button
+				px.form.find(config.buttonUpload + ',' + config.buttonClear).appendTo(pxButton);
+				
+				px.form.hide();
+				this.printForm();
+				
+				//Disable button
+				$(buttonM).attr('disabled','disabled');	
+			},
+			
+			//Clone, format and append form
+			printForm: function(){
+				
+				var formId = 'pxupload' + itr;
+				var iframeId = formId + '_frame';
+				
+				$('<iframe/>').attr({
+					id: iframeId,
+					name: iframeId,
+					src: 'about:blank',
+					style: 'display:none'
+				}).prependTo(pxUploadForm);
+				
+				px.form.clone().attr({
+					id: formId,
+					target: iframeId
+				}).prependTo(pxUploadForm).show();
+				
+				//Show only the file input
+				px.showInputFile( '#'+formId );
+			},
+			
+			//Show only the file input
+			showInputFile: function(form) {
+				$(pxUploadForm).find(form).children().each(function(){
+					isFile = $(this).is(':file');
+					if (!isFile && $(this).find(':file').length == 0) {
+						$(this).hide();
 					}
 				});
-			}
-		});
-		
-		$(".close").live("click", function(){
-			$limit--;
-			if ($limit == config.limit) {
-				px.appendForm();
-			}
-			var id = "#" + $(this).parent().attr("title");
-			$(id+"_frame").remove();
-			$(id).remove();
-			$(id+"_text").fadeOut("slow",function(){
-				$(this).remove();
-			});
-			return false;
-		});
-		
-		$(config.buttonClear).click(function(){
-			$("#px_display").fadeOut("slow",function(){
-				$("#px_display").html("");
-				$("#pxupload_form").html("");
-				itr = 0;
-				$limit = 1;
-				px.appendForm();
-				$('#px_button input').attr("disabled","disabled");
-				$(this).show();
-			});
-		});
-		
-		//private function
-		var px = {
-			init: function(e){
-				var form = $(e).parents('form');
-				px.formAction = $(form).attr('action');
-				$(form).before(' \
-					<div id="pxupload_form"></div> \
-					<div id="px_display"></div> \
-					<div id="px_button"></div> \
-				');
-				$(config.buttonUpload+','+config.buttonClear).appendTo('#px_button');
-				if ( $(e).attr('name') != '' ){
-					config.inputName = $(e).attr('name');
-				}
-				if ( $(e).attr('size') != '' ){
-					config.inputSize = $(e).attr('size');
-				}
-				$(form).hide();
-				this.appendForm();
 			},
-			appendForm: function(){
-				itr++;
-				var formId = "pxupload" + itr;
-				var iframeId = "pxupload" + itr + "_frame";
-				var inputId = "pxupload" + itr + "_input";
-				var contents = '<form method="post" id="'+ formId +'" action="'+ px.formAction +'" enctype="multipart/form-data" target="'+ iframeId +'">' +
-				'<input type="file" name="'+ config.inputName +'" id="'+ inputId +'" class="pxupload" size="'+ config.inputSize +'" onchange="$.fileUploader.change(this);" />' +
-				'</form>' + 
-				'<iframe id="'+ iframeId +'" name="'+ iframeId +'" src="about:blank" style="display:none"></iframe>';
-				
-				$("#pxupload_form").append( contents );
+			//Hide file input and show other data
+			hideInputFile: function(form) {
+				$(pxUploadForm).find(form).children().each(function(){
+					isFile = $(this).is(':file');
+					if (isFile || $(this).find(':file').length > 0) {
+						$(this).hide();
+					} else {
+						$(this).show();
+					}
+				});
 			},
+			
+			//Validate file
 			validateFile: function(file) {
 				if (file.indexOf('/') > -1){
 					file = file.substring(file.lastIndexOf('/') + 1);
 				}else if (file.indexOf('\\') > -1){
 					file = file.substring(file.lastIndexOf('\\') + 1);
 				}
-				//var extensions = /(.jpg|.jpeg|.gif|.png)$/i;
+				
 				var extensions = new RegExp(config.allowedExtension + '$', 'i');
 				if (extensions.test(file)){
 					return file;
 				} else {
 					return -1;
 				}
+			},
+			
+			//clear form
+			clearFormData: function(form) {
+				$(form).find(':input').each(function() {
+					switch(this.type) {
+						case 'file':
+						case 'password':
+						case 'select-multiple':
+						case 'select-one':
+						case 'text':
+						case 'textarea':
+							$(this).val('');
+							break;
+						case 'checkbox':
+						case 'radio':
+							this.checked = false;
+					}
+				});
 			}
+			
 		}
 		
-		px.init(this);
+		//initialize
+		px.init();
+		
+		/*
+		*	Events
+		*/
+		
+		$(this).live('change', function() {
+			
+			//remove upload text after uploaded
+			var uploadText = $(pxUploadForm).data('upload');
+			if (uploadText) {
+				$('.uploadData', pxUploadForm).remove();
+				$(pxUploadForm).removeData('upload');
+			}
+			
+			var $form = $(this).parents('form');
+			
+			//validate file
+			var filename = px.validateFile( $(this).val() );
+			if (filename == -1){
+				alert ('Invalid file!');
+				$(e).val('');
+				return false;
+			}
+			
+			//remove disabled attr
+			$(buttonM).removeAttr('disabled');
+			
+			var loader = 'Uploading...';
+			if ($.trim(config.imageLoader) != ''){
+				loader = '<img src="'+ config.imageLoader +'" alt="uploader" />';
+			}
+			
+			var display = ' \
+				<div class="uploadData" id="pxupload'+ itr +'_text" title="pxupload'+ itr +'"> \
+					<div class="close">&nbsp;</div> \
+					<span class="fname">'+ filename +'</span> \
+					<span class="loader" style="display:none">'+ loader +'</span> \
+					<div class="status">Pending...</div> \
+				</div> \
+			';
+			$(pxUploadForm).append(display);
+			$form.appendTo(pxUploadForm + ' #pxupload'+ itr +'_text');
+			
+			//hide the input file
+			px.hideInputFile( '#' + $form.attr('id') );
+			
+			//increment for printing form
+			itr++;
+			
+			//Limit
+			if (!isLimit) {
+				px.printForm();
+			} else if (--limit) {
+				px.printForm();
+			}
+			
+			//Callback on file Changed
+			config.onFileChange(this, $form);
+		});
+		
+		/*
+		*	Process form Upload
+		*/
+		$(config.buttonUpload, pxButton).click(function(){
+			$(buttonM).attr('disabled','disabled');
+			
+			//trigger before upload callback
+			config.beforeUpload(e);
+			
+			//a variable that counts all the files that are uploaded
+			var fileTobeUpLoaded = 0;
+			
+			$(pxUploadForm + ' form').each(function(){
+				$this = $(this);
+				
+				var id = pxUploadForm + ' #' + $this.attr('id');
+				var filename = $(this).find(selector).val();
+				
+				if (filename != ''){
+					
+					//increment file to be uploaded
+					fileTobeUpLoaded++;
+					
+					$(pxUploadForm).data('upload', true);
+					
+					//trigger before upload callback
+					config.beforeEachUpload($this);
+					
+					$(id + '_text .status').text('Uploading...');
+					$(id + '_text').css('background-color', '#FFF0E1');
+					$(id + '_text .loader').show();
+					$(id + '_text .close').hide();
+					
+					$(id).submit();
+					$(id +'_frame').load(function(){
+						$(id + '_text .loader').hide();
+						output = $(this).contents().find('#output').text();
+						
+						//trigger after each upload
+						config.afterEachUpload($this, output);
+						
+						if (output == 'success'){
+							$(id + '_text').css('background-color', '#F0F8FF');
+							output = config.successOutput;
+						} else {
+							$(id + '_text').css('background-color', '#FF0000');
+							output = config.errorOutput;
+						}
+						output += '<br />' + $(this).contents().find('#message').text();
+						$(id + '_text .status').html(output);
+						
+						$this.remove();
+						$(id + '_frame').remove();
+						
+						$(config.buttonClear, pxButton).removeAttr('disabled');
+						$(selector).removeAttr('disabled');
+						
+						//decrement file to be uploaded
+						fileTobeUpLoaded--
+						if (fileTobeUpLoaded == 0) {
+							//trigger after upload is all executed
+							config.afterUpload(e);
+						}
+					});
+				} else {
+					//disable all file
+					$(this).find(selector).attr('disabled','disabled');
+				}
+			});
+		});
+		
+		//Button Clear Event
+		$(config.buttonClear, pxButton).click(function(){
+			$(pxUploadForm).fadeOut('slow',function(){
+				$(this).empty();
+				$(this).show();
+				
+				itr = 1; //reset iteration
+				limit = parseInt(config.limit);
+				
+				//print the First form
+				px.printForm();
+				
+				//disable button
+				$(buttonM).attr('disabled','disabled');				
+			});
+		});
+		
+		$('.close', pxUploadForm).live('click', function(){
+			
+			if (isLimit) {
+				if (limit == 0) {
+					px.printForm();
+				}
+				limit++;
+			}
+			
+			var id = pxUploadForm + ' #' + $(this).parent().attr('title');
+			$(id+'_text').fadeOut('slow',function(){
+				$(id+'_frame').remove();
+				$(this).remove();
+				
+				//disable button
+				if ($(pxUploadForm).find('form').length <= 1) {
+					$(buttonM).attr('disabled','disabled');	
+				}
+			});
+			
+			//on file remove callback
+			config.onFileRemove(this);
+			
+			return false;
+		});
 		
 		return this;
 	}
